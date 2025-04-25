946d4c4581612b3193b847a398c51e6092667ad2
dhobalenamrata <namratadhobale38939@gmail.com>
2025-04-25 19:05:24 +0530

    improved task01 by TL

--- GitDemo/src/main/java/nit/GitDemo/App.java
+++ GitDemo/src/main/java/nit/GitDemo/App.java
@@ -7,7 +7,11 @@ public class App {
 	static int sum(int x, int y) {
 		return x+y;
 	}
+	static int divition(int x, int y) {
+		return x-y;
+	}
 	public static void main(String[] args) {
         System.out.println("Result : "+sum(15, 45));
+        System.out.println("Result : "+divition(15, 45));
     }
 }
